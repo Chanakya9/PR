c8f60e0eeb271d96687b7dd92a0764212cc77c89
PCReddy <chanakyareddy919@gmail.com>
2016-12-03 12:56:16 -0600

    Chanakya 1st

---
+++ src/buildTools/GmailLoginTest.java
@@ -0,0 +1,18 @@
+package buildTools;
+import org.testng.annotations.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GmailLoginTest {
+/*	 System.setProperty("webdriver.gecko.driver","C:/Dell Laptop_Chan/My Data/QA docs/Selenium_RahulShetty/geckodriver-v0.11.1-win64/geckodriver.exe");
+     WebDriver driver = new FirefoxDriver();
+     driver.get("http://www.google.com");
+     System.out.println(driver.getTitle());*/
+
+//System.out.println("Working");
+	@Test
+	public void run(){
+		System.out.println("Working");
+		
+	}
+}
